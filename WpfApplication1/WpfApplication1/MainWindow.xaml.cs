41509955064b90ca00dbb54b944f9622b5d95ac9
vlazuga <hanex360@gmail.com>
2018-03-14 15:18:51 -0600

    Mover archivos a un destino
    Se utiliza para mover archivos de cualquier tipo de un lugar a otro
    dentro de nuestro equipo

---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics; //Para el path
+using System.Windows.Forms;
+using System.IO;
+ 
+
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string pathArchivo1;
+        string pathDestino1;
+        string nombreArchivo1;
+
+        string pathArchivo2;
+        string pathDestino2;
+        string nombreArchivo2;
+
+        string pathArchivo3;
+        string pathDestino3;
+        string nombreArchivo3;
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Bloquear texbox para que no se pueda escribir
+        //Validar que sean PDF, TXT Y RTF ó DOC, Campo 1 - pdf, campo 2 - txt, campo 3 - Doc Y que sean 3 archivos forzósamente.
+        //Crear repo y subir este codigo
+        //hacer cambios en el local y subirlo nuevamente
+        //Respalda
+        //Mandar liga del repo al patron
+
+    //Para archivo 1
+        private void botonArchivo1_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog findFile1 = new OpenFileDialog();
+
+            if (findFile1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                texboxArchivo1.Text = findFile1.FileName;
+                pathArchivo1 = texboxArchivo1.Text;
+                nombreArchivo1 = System.IO.Path.GetFileName(pathArchivo1);
+                
+            }
+
+        }
+
+        private void botonDestino1_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog findFolder1 = new FolderBrowserDialog();
+
+            if (findFolder1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                texboxDestino1.Text = findFolder1.SelectedPath;
+                pathDestino1 = texboxDestino1.Text + "\\" + nombreArchivo1;
+            }
+        }
+
+         private void botonMover1_Click(object sender, RoutedEventArgs e)
+        { 
+             //try
+                File.Move(pathArchivo1, pathDestino1);
+
+                System.Windows.Forms.MessageBox.Show("Se ha movido el archivo de " + pathArchivo1 + " a " + pathDestino1);
+                texboxDestino1.Text = "";
+                texboxArchivo1.Text = "";
+             //catch
+             //pruebas con folder en el que no se tenga acceso para moverlo
+
+
+            
+        }
+
+
+    //Para archivo 2
+         private void botonArchivo2_Click(object sender, RoutedEventArgs e)
+         {
+             OpenFileDialog findFile2 = new OpenFileDialog();
+
+             if (findFile2.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+             {
+                 texboxArchivo2.Text = findFile2.FileName;
+                 pathArchivo2 = texboxArchivo2.Text;
+                 nombreArchivo2 = System.IO.Path.GetFileName(pathArchivo2);
+
+             }
+
+         }
+
+         private void botonDestino2_Click(object sender, RoutedEventArgs e)
+         {
+             FolderBrowserDialog findFolder2 = new FolderBrowserDialog();
+             
+             if (findFolder2.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+             {
+                 texboxDestino2.Text = findFolder2.SelectedPath;
+                 pathDestino2 = texboxDestino2.Text + "\\" + nombreArchivo2;
+             }
+         }
+
+         private void botonMover2_Click(object sender, RoutedEventArgs e)
+         {
+             File.Move(pathArchivo2, pathDestino2);
+
+             System.Windows.Forms.MessageBox.Show("Se ha movido el archivo de " + pathArchivo2 + " a " + pathDestino2);
+             texboxDestino2.Text = "";
+             texboxArchivo2.Text = "";
+         }
+
+
+
+      //Para archivo 3
+         private void botonArchivo3_Click(object sender, RoutedEventArgs e)
+         {
+             OpenFileDialog findFile3 = new OpenFileDialog();
+
+             if (findFile3.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+             {
+                 texboxArchivo3.Text = findFile3.FileName;
+                 pathArchivo3 = texboxArchivo3.Text;
+                 nombreArchivo3 = System.IO.Path.GetFileName(pathArchivo3);
+
+             }
+
+         }
+
+         private void botonDestino3_Click(object sender, RoutedEventArgs e)
+         {
+             FolderBrowserDialog findFolder3 = new FolderBrowserDialog();
+
+             if (findFolder3.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+             {
+                 texboxDestino3.Text = findFolder3.SelectedPath;
+                 pathDestino3 = texboxDestino3.Text + "\\" + nombreArchivo3;
+             }
+         }
+
+         private void botonMover3_Click(object sender, RoutedEventArgs e)
+         {
+             File.Move(pathArchivo3, pathDestino3);
+
+             System.Windows.Forms.MessageBox.Show("Se ha movido el archivo de " + pathArchivo3 + " a " + pathDestino3);
+             texboxDestino3.Text = "";
+             texboxArchivo3.Text = "";
+         }
+
+
+
+
+
+
+        private void botonSalir_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        
+
+
+
+
+
+
+
+
+
+
+        private void texboxArchivo1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+           
+        }
+
+        private void texboxDestino1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        
+    }
+}
